7e140ef20ab3f72223d61e42b4a382f93e5d51d8
hozumi <tvxqg5jyj@gmail.com>
2020-05-29 11:50:15 +0900

    수정

--- EntryFeeCalculation.java
+++ EntryFeeCalculation.java
@@ -2,23 +2,23 @@
 /**
  * Write a description of class EntryFeeCalculation here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (스피겔크릴,노무라타카미치,호즈미요시아키)
+ * @version (2020/05/29)
  */
 public class EntryFeeCalculation
 {
     public String entryFee(int age){
         String result = "Error";
 
-        //유아(4세이하)
+        //유아(4세미만)
         if(age >=0 && age <= 3){
             result = "0원";
         }
-        //어린이(7세이상)
+        //어린이(4세이상)
         else if(age >= 4 && age <= 7){
             result = "400원";
         }
-        //초중학생()
+        //초중학생(7세이상)
         else if(age >= 7 && age <= 15){
             result = "900원";
         }
