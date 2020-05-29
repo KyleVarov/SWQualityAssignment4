1488bf7fa20f000b142558499c753d2efcdd732a
Kylevarov <kyrill.spiegl@gmail.com>
2020-05-29 11:14:15 +0900

    EntrFeeCalculation class + entryFee Method initialized

---
+++ EntryFeeCalculation.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class EntryFeeCalculation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class EntryFeeCalculation
+{
+    public String entryFee(int age){
+        String result = "";
+        
+        return result;
+    }
+}
