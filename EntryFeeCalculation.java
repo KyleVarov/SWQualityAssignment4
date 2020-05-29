de8cc8ccda6c13d6982294770232e7067122d5b7
Kylevarov <kyrill.spiegl@gmail.com>
2020-05-29 11:39:36 +0900

    entryfee method fix

--- EntryFeeCalculation.java
+++ EntryFeeCalculation.java
@@ -8,8 +8,19 @@
 public class EntryFeeCalculation
 {
     public String entryFee(int age){
-        String result = "";
+        String result = "Error";
         
-        return result;
-    }
+         if(age >=0 && age <= 3){
+             result = "0원";
+         }
+         else if(age >= 4 && age <= 7){
+             result = "400원";
+         }else if(age >= 7 && age <= 15){
+             result = "900원";
+         }else if(age >= 16 && age<=130){
+             result = "2000원";
+         }
+        
+         return result;
+        }
 }
