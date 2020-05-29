5cbd8ea2b892492d39cece8feeb7626b63baa306
NomuraTakamichi <kennn3065@gmail.com>
2020-05-29 11:40:29 +0900

    push

--- EntryFeeCalculation.java
+++ EntryFeeCalculation.java
@@ -8,19 +8,19 @@
 public class EntryFeeCalculation
 {
     public String entryFee(int age){
-     String result = "Error";
+        String result = "Error";
             
-             if(age >=0 && age <= 3){
-                 result = "0원";
-             }
-             else if(age >= 4 && age <= 7){
-                 result = "400원";
-             }else if(age >= 7 && age <= 15){
-                 result = "900원";
-             }else if(age >= 16 && age<=130){
-                 result = "2000원";
-             }
+        if(age >=0 && age <= 3){
+            result = "0원";
+        }
+        else if(age >= 4 && age <= 7){
+            result = "400원";
+        }else if(age >= 7 && age <= 15){
+            result = "900원";
+        }else if(age >= 16 && age<=130){
+            result = "2000원";
+        }
             
-     return result;
+        return result;
     }
 }
