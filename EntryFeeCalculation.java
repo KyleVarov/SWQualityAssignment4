e45d8e304c4c79cc20414de175747f8030217dd7
hozumi <tvxqg5jyj@gmail.com>
2020-06-03 12:05:19 +0900

    주소문 적어놓았습니다.

--- EntryFeeCalculation.java
+++ EntryFeeCalculation.java
@@ -1,12 +1,16 @@
 
 /**
- * Write a description of class EntryFeeCalculation here.
+ * 수족관 입장료를 고객님의 나이에 따라 계산하는 프로그램.
  *
  * @author (스피겔크릴,노무라타카미치,호즈미요시아키)
- * @version (2020/05/29)
+ * @version (2020/06/03)
  */
 public class EntryFeeCalculation
 {
+   
+    /**
+     * 나이를 int로 입력받아서 입장료를 String로 return하는 메소드이다.
+     */
     public String entryFee(int age){
         String result = "Error";
 
